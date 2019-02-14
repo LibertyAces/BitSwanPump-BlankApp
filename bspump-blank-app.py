4ba466144ee45d9a3511d9046917e08e440904bf
Ales Teska <ales.teska@teskalabs.com>
2019-02-14 10:21:42 +0100

    Blank app adjustments.

--- bspump-blank-app.py
+++ bspump-blank-app.py
@@ -1,45 +1,28 @@
 #!/usr/bin/env python3
 import sys
-
-import logging
 import asyncio
-import asab
 import bspump
-import bspump.socket
 import bspump.common
-import bspump.trigger
-from  bspump.abc.processor import Processor
+import bspump.socket
 
-###
 
-L = logging.getLogger(__name__)
-
-###
-
-class BlankPipeline(bspump.Pipeline):
+class MyPipeline(bspump.Pipeline):
 
 	def __init__(self, app, pipeline_id):
 		super().__init__(app, pipeline_id)
 
-		# source_config = {'path': 'sample.csv', 'delimiter': ','}
-
 		self.build(
-			bspump.socket.TCPSource (app, self, config={"host":"0.0.0.0", "port": 8888}),
+			bspump.socket.TCPSource(app, self, config={"host":"0.0.0.0", "port": 8888}),
 			ShakespeareanEnricher(app, self),
-			bspump.common.PPrintSink(app, self)
+			bspump.common.PPrintSink(app, self, stream=sys.stderr)
 		)
 
 
-
-class ShakespeareanEnricher(Processor):
+class ShakespeareanEnricher(bspump.Processor):
 
 	def process(self, context, event):
-
-		msg = event.decode("utf-8").replace ('\n','')
-		s_msg = 'To {0}, or not to {0}?'.format (msg)
-		return s_msg
-
-
+		msg = event.decode("utf-8").replace('\n','')
+		return 'To {0}, or not to {0}?'.format(msg)
 
 
 if __name__ == '__main__':
@@ -47,8 +30,7 @@ if __name__ == '__main__':
 
 	svc = app.get_service("bspump.PumpService")
 
-	# Construct and register Pipeline
-	pl = BlankPipeline(app, 'blank_pipeline')
+	pl = MyPipeline(app, 'MyPipeline')
 	svc.add_pipeline(pl)
 
 	app.run()
